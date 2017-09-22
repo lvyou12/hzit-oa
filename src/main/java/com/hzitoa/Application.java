f6bb6c20c7d667385a5ec12b5e25bd5596d0caad
lvyou12 <983566073@qq.com>
2017-09-22 10:15:53 +0800

    初始化项目

---
+++ src/main/java/com/hzitoa/Application.java
@@ -0,0 +1,19 @@
+package com.hzitoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author 吕游
+ * @company 合众艾特
+ * @create 2017-09-22 10:05
+ * @description
+ */
+@ComponentScan(value = {"com.hzitoa.web"})
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/com/hzitoa/web/JumpController.java
@@ -0,0 +1,18 @@
+package com.hzitoa.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 吕游
+ * @company 合众艾特
+ * @create 2017-09-22 10:04
+ * @description
+ */
+@Controller
+public class JumpController {
+    @RequestMapping("/index")
+    public String toIndex(){
+        return "index";
+    }
+}
