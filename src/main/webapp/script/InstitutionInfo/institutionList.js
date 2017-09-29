eee6f56c6d62b20c1de3e4ce80bfdcc90ef4f35b
lvyou12 <983566073@qq.com>
2017-09-29 08:30:17 +0800

    添加数据库制度表中数据的异步展示

--- src/main/webapp/script/InstitutionInfo/institutionList.js
+++ src/main/webapp/script/InstitutionInfo/institutionList.js
@@ -1,5 +1,39 @@
 $(function(){
 
+    var json ={
+        url: "/institutionInfo/listData",
+        toolbar: '#toolBar',                //工具按钮用哪个容器
+        pagination: true,                   //是否显示分页（*）
+        striped: true,                      //是否显示行间隔色
+        sidePagination: "server",           //分页方式：client学员端分页，server服务端分页（*）
+        idField: 'instId',
+        pageNumber: 1,                       //初始化加载第一页，默认第一页
+        pageSize: 10,                       //每页的记录行数（*）
+        pageList: [10, 15, 20, 25],        //可供选择的每页的行数（*）
+        //clickToSelect: true,                //是否启用点击选中行
+        smartDisplay: false, // 智能显示 pagination 和 cardview 等
+        //exportDataType: "basic",              //basic', 'all', 'selected'.
+        showRefresh: true,
+        showColumns: true,
+        //detailView: true,
+
+        columns: [{
+            filed: 'instId',
+            title: '编号',
+            checkbox: true
+        }, {
+            field: 'name',
+            title: '制度名',
+            width: 80
+        }]
+    }
+
+    showInsitutionInfo();
+    function showInsitutionInfo() {
+        //getUrl();
+        $("#table").bootstrapTable('destroy');//先要将table销毁，否则会保留上次加载的内容
+        $table = $('#table').bootstrapTable(json);
+    }
 
     /**
      * 制度上传
--- src/main/webapp/script/home.js
+++ src/main/webapp/script/home.js
@@ -3,27 +3,48 @@
  */
 $(function(){
 
-    $.getJSON("/institutionInfo/listData?offset=1&limit=9",function(resp){
+    $.getJSON("/institutionInfo/listData?offset=1&limit=6",function(resp){
+        console.log(resp);
+        var result = "<span id='pin' class='glyphicon glyphicon-pushpin' aria-hidden='true'></span>"+
+        "<span class='list-title'>部门最新制度</span>";
+        for(var i = 0; i < resp.rows.length; i++){
+            //console.log(i%3 == 0)
+            result += "<li>";
+            if(i % 3 == 0){
+                result += "<span class='num1'>"+(i+1)+"</span>";
+            }
+            if(i % 3 == 1){
+                result += "<span class='num2'>"+(i+1)+"</span>";
+            }
+            if(i % 3 == 2){
+                result += "<span class='num3'>"+(i+1)+"</span>";
+            }
+            result += "<a name='/"+resp.rows[i].path+resp.rows[i].name+"'>"+resp.rows[i].name+"</a>";
+            result += "</li>";
+        }
+        result += "<span id='showMore' class='more'>查看更多...</span>"
+        $(".sideList").html(result);
 
-    });
-
-    $("#showMore").click(function(){
-        var ifm = parent.document.getElementById("myFrame");
-        ifm.src = "/institutionInfo/institutionList";
-    });
+        $("#showMore").on("click",function(){
+            var ifm = parent.document.getElementById("myFrame");
+            ifm.src = "/institutionInfo/institutionList";
+        });
 
 
-    $(".sideList > li > a").each(function(){
-        var _this = $(this);
-        _this.on("click",function(){
-            var path = _this.attr("name");
-            layer.open({
-                type: 2,
-                title: "制度预览",
-                content: "/institutionInfo/showInstitution?path="+path,
-                area:['860px','670px'],
-                offset:['20px']
-            })
+        $(".sideList > li > a").each(function(){
+            var _this = $(this);
+            _this.on("click",function(){
+                var path = _this.attr("name");
+                layer.open({
+                    type: 2,
+                    title: "制度预览",
+                    content: "/institutionInfo/showInstitution?path="+path,
+                    area:['860px','670px'],
+                    offset:['20px']
+                })
+            });
         });
     });
+
+
 });
