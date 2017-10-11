b80477936d9540f84d2cc6d3ddafc7b305f90197
lvyou12 <983566073@qq.com>
2017-10-11 11:00:58 +0800

    修改员工表和权限表

--- src/main/java/com/hzitoa/entity/TbAuthority.java
+++ src/main/java/com/hzitoa/entity/TbAuthority.java
@@ -1,11 +1,13 @@
 package com.hzitoa.entity;
 
 import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
 
 import java.io.Serializable;
+import java.util.Date;
 
 
 /**
@@ -40,6 +42,26 @@ public class TbAuthority extends Model<TbAuthority> {
      */
 	private Integer available;
 	private String permission;
+	/**
+	 * 创建人
+	 */
+	@TableField("create_by")
+	private String createBy;
+	/**
+	 * 创建时间
+	 */
+	@TableField("create_time")
+	private Date createTime;
+	/**
+	 * 修改人
+	 */
+	@TableField("update_by")
+	private String updateBy;
+	/**
+	 * 修改时间
+	 */
+	@TableField("update_time")
+	private Date updateTime;
 
 
 	public TbAuthority() {
@@ -93,6 +115,38 @@ public class TbAuthority extends Model<TbAuthority> {
 		this.permission = permission;
 	}
 
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
 	@Override
 	protected Serializable pkVal() {
 		return this.authId;
@@ -107,6 +161,10 @@ public class TbAuthority extends Model<TbAuthority> {
 				", pid=" + pid +
 				", available=" + available +
 				", permission='" + permission + '\'' +
+				", createBy='" + createBy + '\'' +
+				", createTime=" + createTime +
+				", updateBy='" + updateBy + '\'' +
+				", updateTime=" + updateTime +
 				'}';
 	}
 }
